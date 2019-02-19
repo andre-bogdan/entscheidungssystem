2e376b09b4665ac36d85b9bef672cd418b5c0dca
Andre Bogdan <abogdan@web.de>
2019-02-19 12:50:11 +0100

    20190219-1

--- src/entscheidungssystem/Controller/Controller.java
+++ src/entscheidungssystem/Controller/Controller.java
@@ -1,5 +1,17 @@
 package entscheidungssystem.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
 public class Controller {
 
+    //Initialisierung der Oberflaeche
+    /*public void initialize(){
+        fxRootPane.getChildren().clear();
+        fxRootPane.getChildren().add(fxPane_1);
+    }*/
+
+    public void progBeenden(){
+        System.exit(0);
+    }
 }
