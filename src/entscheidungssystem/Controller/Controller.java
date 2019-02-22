b9f1332fed0e5606a02423af502c449ff3b6583a
Andre Bogdan <abogdan@web.de>
2019-02-22 12:53:04 +0100

    20190222-1

--- src/entscheidungssystem/Controller/Controller.java
+++ src/entscheidungssystem/Controller/Controller.java
@@ -2,16 +2,35 @@ package entscheidungssystem.Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 public class Controller {
+    @FXML
+    Pane fxPaneRoot, fxPane_1, fxPane_2;
 
     //Initialisierung der Oberflaeche
-    /*public void initialize(){
-        fxRootPane.getChildren().clear();
-        fxRootPane.getChildren().add(fxPane_1);
-    }*/
+    public void initialize(){
+        fxPaneRoot.getChildren().clear();
+        fxPaneRoot.getChildren().add(fxPane_1);
+    }
 
+    //MenueItem beenden
     public void progBeenden(){
         System.exit(0);
+    }
+
+    //Button weiter auf GridPane_1
+    public void weiter_12(){
+        fxPaneRoot.getChildren().clear();
+        fxPaneRoot.getChildren().add(fxPane_2);
+    }
+
+    /*public void weiter_23(){
+
+    }*/
+
+    public void zurueck_2_to_1(){
+
     }
 }
